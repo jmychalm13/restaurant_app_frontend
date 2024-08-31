ff1cb09a5aa991cfa5a7f0e14a68d7b16545fc19
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-31 07:56:14 -0500

    ordersIndex component rendering data

--- src/OrdersIndex.jsx
+++ src/OrdersIndex.jsx
@@ -1,9 +1,28 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 export function OrdersIndex() {
+  const [orders, setOrders] = useState([]);
+
+  const handleGetOrders = () => {
+    axios.get("http://localhost:3000/orders.json").then((response) => {
+      console.log(response.data);
+      setOrders(response.data);
+    });
+  };
+
+  useEffect(handleGetOrders, []);
+
   return (
     <div>
       <h1>All Orders</h1>
+      <div>
+        {orders.map((order) => (
+          <div key={order.id}>
+            <p>{order.status}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
