51a9788fb0674a65ab57c378123c5566cb39b3d6
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-09-01 10:25:24 -0500

    added beginning of placing order logic

--- src/MenuItemsIndex.jsx
+++ src/MenuItemsIndex.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 export function MenuItemsIndex() {
   const [menuItems, setMenuItems] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
 
   const handleGetMenuItems = () => {
     axios.get("http://localhost:3000/menu_items.json").then((response) => {
@@ -11,17 +12,36 @@ export function MenuItemsIndex() {
     });
   };
 
+  const handleAddToOrderList = (item) => {
+    if (item.availability) {
+      setSelectedItems((prevItems) => [...prevItems, item]);
+    }
+  };
+
+  const handleSubmitOrder = () => {
+    console.log("triggered");
+  };
+
   useEffect(handleGetMenuItems, []);
 
   return (
     <div>
-      <h1>Menu</h1>
-      <div>
+      <h1>Create Order</h1>
+      <div className="container p-6">
         {menuItems.map((item) => (
-          <div key={item.id}>
-            <p>{item.name}</p>
+          <div
+            key={item.id}
+            className={`flex justify-between ${!item.availability ? "text-gray-500" : "text-black cursor-pointer"}`}
+          >
+            <p onClick={item.availability ? () => handleAddToOrderList(item) : null}>{item.name}</p>
+            <p className="font-bold">${item.price}</p>
           </div>
         ))}
+      </div>
+      <div className="w-full flex items-center">
+        <button type="submit" onClick={handleSubmitOrder}>
+          Place Order
+        </button>
       </div>
     </div>
   );
